d6a2b42aefa6ed607941c6621edef6b9dcdd340d
Tiago <t@t.com>
2021-11-30 13:40:07 +0000

    after release preparation

--- tests/ts-workspace.test.ts
+++ tests/ts-workspace.test.ts
@@ -1,6 +1,12 @@
+import {getOpenDSU} from "../../demo-repositories/lib";
 
 describe(`Type Script Workspace test`, function(){
     it(`Tests Belong in the dsu-blueprint-workspace repository`, function(){
+        try{
+            getOpenDSU()
+        } catch (e) {
+            expect(e).toBeDefined();
+        }
         expect(1).toBe(1);
     });
 });
