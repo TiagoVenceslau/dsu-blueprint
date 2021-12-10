2e9e176ea4788e79bec3061c30c3cae11aac209b
tiago <t@t.com>
2021-12-10 09:48:15 +0000

    refs #1 - updates cli to allow for direct build from cmd line

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -49,6 +49,12 @@ export function getEnvJs(app: string, pathToApps: string, callback: ObjectCallba
     });
 }
 
+/**
+ * Builds a DSU from a transpiled {@link DSUBlueprint} file
+ * @param {CliOptions} config the cli options
+ * @param {Callback} callback
+ * @namespace cli
+ */
 export function build(config: CliOptions, callback: Callback){
     let blueprintFile;
     try {
@@ -71,15 +77,48 @@ export function build(config: CliOptions, callback: Callback){
     });
 }
 
+/**
+ * Accepted params to the cli interface
+ * @namespace cli
+ */
 export type CliOptions = {
+    /**
+     * one of {@link CliActions}. defaults to {@link CliActions.BUILD}
+     */
     action: CliActions,
+    /**
+     * the domain to anchor the dsu
+     */
     domain?: string,
+    /**
+     * The location of the transpiled build Blueprint file. defaults to './build/build.js'
+     */
     blueprint: string,
+    /**
+     * used in conjunction with {@link pathToOpenDSU}
+     *
+     * prefixes {@link pathToOpenDSU} in older to control the path, depending on where the cli file is located
+     * defaults to '../../../../../' (when it's ran from 'node_modules/@tvenceslau/dsu-blueprint/lib/cli')
+     */
     pathAdaptor: string,
+    /**
+     * used in conjunction with {@link pathAdaptor}
+     *
+     * set's the relative path to the OpenDSU bundle file.
+     * defaults to '../privatesky/psknode/bundles/openDSU.js'
+     */
     pathToOpenDSU: string
 }
 
+/**
+ * Available Actions the Cli supports
+ * @namespace cli
+ */
 export enum CliActions {
+    /**
+     * Builds Or Updates a DSU from a blueprint file
+     * (will delete the contents of the DSU beforehand)
+     */
     BUILD = 'build',
     UPDATE = 'update'
 }
@@ -88,7 +127,7 @@ const defaultOptions: CliOptions = {
     action: CliActions.BUILD,
     domain: "default",
     blueprint: "./build/build.js",
-    pathAdaptor: './',
+    pathAdaptor: '../../../../../', // this assumes this is running from node_modules/@tvenceslau/dsu-blueprint/lib/cli
     pathToOpenDSU: '../privatesky/psknode/bundles/openDSU.js'
 }
 
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,13 @@
 /**
+ * Exposes a Command Line interface to interact with transpiled {@link DSUBlueprint}s
+ *
+ * Available actions: {@link CliActions}
+ *
+ * Available options: {@link CliOptions} eg for option 'action':
+ * <pre>
+ *     node ./node_modules/@tvenceslau/dsu-blueprint/lib/cli --action=build
+ * </pre>
+ *
  * @namespace cli
  * @memberOf dsu-blueprint
  */
