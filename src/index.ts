ac2a2eec494f54204806f5d7f349cd84ca5ad720
Tiago <tiago@email.com>
2021-12-14 22:40:15 +0000

    refs #2 - mermaid doc test

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,43 @@
  * A decorator based approach to OpenDSU creation and management
  *
  * @module dsu-blueprint
+ *
  */
+
+
+
+
+/**
+ * Sequence Diagram for a Single {@link DSU} creation:
+ * @class Teest
+ *
+ * @memberOf dsu-blueprint
+ *
+ * @mermaid
+ *      sequenceDiagram
+ *          System->>Repository:create(DSUModel, ...keyArgs, callback)
+ *          Repository->>RepositoryLoop:onCreate(DSUModel, callback)
+ *          RepositoryLoop->>RepositoryUtils.createFromDecorators(DSUModel)
+ *          RepositoryUtils->>DSUPhaseLoop:handleCreationDecorators(DSUModel, decorators, callback)
+ *          loop CreationDecorators
+ *              DSUPhaseLoop->>Repository:create(DSUModel, ...keyArgs, callback)
+ *              Repository->>DSUPhaseLoop: Updated DSUModel, created DSU
+ *              DSUPhaseLoop->>DSUPhaseLoop: Cache DSU
+ *          end
+ *          DSUPhaseLoop->>RepositoryUtils: Updated DSUModel and cached DSUs
+ *          RepositoryUtils->>DSUPhaseLoop:handleClassDecorators(DSUModel, decorators, callback);
+ *          DSUPhaseLoop->>RepositoryUtils: Updated DSUModel and DSU
+ *          RepositoryUtils->>DSUPhaseLoop:handleEditDecorators(DSUModel, cachedDSUs, dsu, decorators, callback)
+ *          loop EditDecorators
+ *              DSUPhaseLoop->>DSU:edit()
+ *          end
+ *          DSUPhaseLoop->>RepositoryUtils: Final DSUModel, DSU and KeySSI
+ *          RepositoryUtils->>RepositoryLoop:afterCreate(DSUModel, DSU, KeySSI, callback)
+ *          RepositoryLoop->>Repository: Final DSUModel, DSU and KeySSI
+ *          Repository->>System: Final DSUModel, DSU and KeySSI
+ */
+export class Teest{}
+
 
 export * from './core';
 export * from './fs';
